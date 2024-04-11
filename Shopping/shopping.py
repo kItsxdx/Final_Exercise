81b626a9dca4b9ebb2c54231675a57a2ce209b47
kItsxdx <66111375@dpu.ac.th>
2024-04-11 19:48:25 +0700

    base_code-shopping.py

---
+++ Shopping/shopping.py
@@ -0,0 +1,19 @@
+class Item:
+    def __init__(self, item: str, price: float) -> None:
+        pass
+
+
+class MemberCard:
+    def __init__(self, stype: str, cart: str, point: int) -> None:
+        pass
+
+    def add_item(self, item: Item):
+        pass
+
+    def calculate_point():
+        pass
+
+
+class PlatinumCard:
+    def calculate_point():
+        pass
