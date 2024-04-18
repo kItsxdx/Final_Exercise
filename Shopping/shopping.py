9efe43c512252ce52b4db808b470ab2378d73a66
kItsxdx <66111375@dpu.ac.th>
2024-04-18 18:58:32 +0700

    test_1

--- Shopping/shopping.py
+++ Shopping/shopping.py
@@ -1,23 +1,53 @@
 class Item:
-    def __init__(self, item: str, price: float) -> None:
-        pass
+    def __init__(self, item: str, price: float):
+        self.item = item
+        self.price = price
+
+
+"""
+    ฟังก์ชันสร้างวัตถุ Item
+
+    Args:
+      item: ชื่อสินค้า
+      price: ราคาสินค้า
+    """
 
 
 class MemberCard:
-    def __init__(self, stype: str, cart: str, point: int) -> None:
-        pass
+    """
+  คลาสสำหรับจัดเก็บข้อมูลเกี่ยวกับบัตรสมาชิกของร้านประเภททั่วไป
+  """
 
-    def add_item(self, item: Item):
-        pass
+    def __init__(self):
+        """
+    ฟังก์ชันสร้างวัตถุ MemberCard
+    """
+        self.ctype = "Member Card"
+        self.cart = []
+        self.point = 0
 
-    def calculate_point():
-        if price >=25
-        return point+1
-        else:
-        return None
-        
+    def add_item(self, item, price):
+        """ฟังชันเพิ่มสินค้าลงในตะกร้าสินค้า
+        Arge:
+            item: ชื่อสินค้า
+            price: ราคาสินค้า
+        """
+        self.cart.append(Item(item, price))
+
+    def calculate_point(self):
+        """ฟังชั่นคำนวณคะแนนที่ได้รับจากการใช้จ่าย
+            Return:
+                คะแนนที่ได้รับจากการใช้จ่าย        
+        """
+        for item in self.cart:
+            self.point += item.price // 40
 
 
-class PlatinumCard:
+class PlatinumCard(MemberCard):
+    """คลาสสำหรับจัดเก็บข้อมูลเกี่ยวกับบัตรสมาชิกของร้านประเภท platinum"""
+
+    def __init__(self):
+        super().__init__()
+
     def calculate_point():
         pass
